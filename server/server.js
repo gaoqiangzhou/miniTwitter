1865e9acd633b788de50fac667ca63c6fbcf55f2
gaoqiangzhou <gaoqiangzhouer@gmail.com>
2023-10-03 00:31:36 -0400

    new file:   server/.gitignore 	new file:   server/desktop.ini 	new file:   server/package-lock.json 	new file:   server/package.json 	new file:   server/server.js

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res)=>{
+    res.send("Hello")
+})
+
+mongoose.connect('mongodb+srv://zhou:cc12112@cluster0.ebnlf4j.mongodb.net/MiniTwitter?retryWrites=true&w=majority')
+.then(()=>{
+    app.listen(3000, ()=>{
+        console.log("Mini Twitter is running at port 3000")
+    })
+    console.log('connected to MongoDB')
+}).catch((error) => {
+    console.log(error)
+})
