4e10a92672db70c22eb80a3cf09be7817d4dab4c
gaoqiangzhou <gaoqiangzhouer@gmail.com>
2023-10-18 17:26:56 -0400

    added navigation bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,14 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Post from './pages/Post';
+import NavBar from './components/NavBar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
 
   return (
       <div>
         <Router>
+          <NavBar/>
           <Routes>
             <Route exact path="/" element = {<Home/>}/>
             <Route exact path="/login" element = {<Login/>}/>
