89a860e92213715648665495ac7ed2056018469e
gaoqiangzhou <gaoqiangzhouer@gmail.com>
2023-11-06 10:56:50 -0500

    Revert "Post schema update"

--- server/Routes/user.js
+++ server/Routes/user.js
@@ -5,150 +5,138 @@ const User = require("../Models/user");
 //password handle
 const bcrypt = require("bcrypt");
 //env variables
-require("dotenv").config();
+require('dotenv').config()
 //jsonwebtoken
-const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken")
 const createToken = (_id) => {
-  //(payload, secret, options)
-  return jwt.sign({ _id }, process.env.ACCESS_TOKEN_SECRET, {});
-};
+    //(payload, secret, options)
+    return jwt.sign({_id}, process.env.ACCESS_TOKEN_SECRET, {});
+}
 
 //add a new user
 router.post("/register", (req, res) => {
-  let { name, type, password } = req.body;
-  name.trim();
-  password.trim();
+    let {name, type, password} = req.body;
+    name.trim();
+    password.trim();
 
-  if (name === "" || password === "") {
-    res.json({
-      status: "FAILED",
-      message: "Empty input fields",
-    });
-  } else if (!/^[a-zA-Z]*$/.test(name)) {
-    res.json({
-      status: "FAILED",
-      message: "Invalid name enterd",
-    });
-  } else if (password.length < 8) {
-    res.json({
-      status: "FAILED",
-      message: "password is too short",
-    });
-  } else {
-    //check if account already exists
-    User.find({ name })
-      .then((result) => {
-        if (result.length) {
-          res.json({
+    if(name === "" || password ==="")
+    {    res.json({
             status: "FAILED",
-            message: "User already exists",
-          });
-        } else {
-          //add new user and hash the password
-
-          //password hashing
-          const saltRounds = 5;
-          bcrypt
-            .hash(password, saltRounds)
-            .then((hashedPassword) => {
-              const newUser = new User({
-                name,
-                type,
-                password: hashedPassword,
-              });
-              newUser
-                .save()
-                .then((result) => {
-                  //once successfully add a user to the database
-                  //create a token
-                  const token = createToken(result._id);
-                  res.json({
-                    status: "SUCCESS",
-                    message: "User registered sucessful",
-                    data: result,
-                    token: token,
-                  });
-                })
-                .catch((err) => {
-                  res.json({
-                    status: "FAILED",
-                    message: "error happend when saving user account",
-                  });
-                });
-            })
-            .catch((err) => {
-              res.json({
-                status: "FAILED",
-                message: "error happend when hasing the password",
-              });
-            });
-        }
-      })
-      .catch((err) => {
-        console.log(err);
+            message: "Empty input fields"
+    })} else if(!/^[a-zA-Z]*$/.test(name)){
         res.json({
-          status: "FAILED",
-          message: "an error has occured",
-        });
-      });
-  }
+            status: "FAILED",
+            message: "Invalid name enterd"
+        })
+    }  else if(password.length < 8){
+        res.json({
+            status: "FAILED",
+            message: "password is too short"
+        })
+    } else {
+        //check if account already exists
+        User.find({name}).then(result => {
+            if(result.length) {
+                res.json({
+                    status: "FAILED",
+                    message: "User already exists"
+                })
+            }else {
+                //add new user and hash the password
+
+                //password hashing
+                const saltRounds = 5;
+                bcrypt.hash(password, saltRounds).then(hashedPassword => {
+                    const newUser = new User({
+                        name,
+                        type,
+                        password: hashedPassword
+                    });
+                    newUser.save().then(result => {
+                        //once successfully add a user to the database
+                        //create a token
+                        const token = createToken(result._id);
+                        res.json({
+                            status: "SUCCESS",
+                            message: "User registered sucessful",
+                            data: result,
+                            token: token
+                        })
+                    }).catch(err => {
+                        res.json({
+                            status: "FAILED",
+                            message: "error happend when saving user account"
+                        })
+                    })
+                }).catch(err => {
+                    res.json({
+                        status: "FAILED",
+                        message: "error happend when hasing the password"
+                    })
+                })
+            }
+        }).catch(err => {
+            console.log(err);
+            res.json({
+                status: "FAILED",
+                message: "an error has occured"
+            })
+        })
+    }
+    
 });
 //Log in
 router.post("/login", (req, res) => {
-  let { name, password } = req.body;
-  name.trim();
-  password.trim();
-  if (name === "" || password === "") {
-    res.json({
-      status: "FAILED",
-      message: "Empty input fields",
-    });
-  } else {
-    User.find({ name })
-      .then((data) => {
-        if (data) {
-          //user exists
-          const hashedPassword = data[0].password;
-          bcrypt
-            .compare(password, hashedPassword)
-            .then((result) => {
-              if (result) {
-                //password matchs
-                //create a token for the user
-                const token = createToken(data._id);
-                res.json({
-                  status: "SUCCESS",
-                  message: "login success",
-                  data: data,
-                  token: token,
-                });
-              } else {
-                res.json({
-                  status: "FAILED",
-                  message: "invalid password entered",
-                });
-              }
-            })
-            .catch((err) => {
-              res.json({
-                status: "FAILED",
-                message: "error happens when comparing the password",
-              });
-            });
-        } else {
-          //user not exists
-          res.json({
-            status: "FAILED",
-            message: "User not exists",
-          });
-        }
-      })
-      .catch((err) => {
+    let {name, password} = req.body;
+    name.trim();
+    password.trim();
+    if(name ==="" || password ==="")
+    {
         res.json({
-          status: "FAILED",
-          message: "error happens when finding the user",
-        });
-      });
-  }
-});
+            status: "FAILED",
+            message: "Empty input fields"
+        })
+    }else{
+        User.find({name}).then(data => {
+            if(data){
+                //user exists
+                const hashedPassword = data[0].password;
+                bcrypt.compare(password, hashedPassword).then(result => {
+                    if(result){
+                        //password matchs
+                        //create a token for the user
+                        const token = createToken(data._id);
+                        res.json({
+                            status: "SUCCESS",
+                            message: "login success",
+                            data: data,
+                            token: token
+                        })
+                    } else {
+                        res.json({
+                            status: "FAILED",
+                            message: "invalid password entered"
+                        })
+                    }
+                }).catch(err => {
+                    res.json({
+                        status: "FAILED",
+                        message: "error happens when comparing the password"
+                    })
+                })
+            } else{
+                //user not exists
+                res.json({
+                    status: "FAILED",
+                    message: "User not exists"
+                })
+            }
+        }).catch(err => {
+            res.json({
+                status: "FAILED",
+                message: "error happens when finding the user"
+            })
+        })
+    }
+})
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,24 +1,22 @@
-const express = require("express");
+const express = require('express')
 const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
-const userRoute = require("./Routes/user");
+const cors = require('cors');
+const mongoose = require('mongoose');
+const userRoute = require("./Routes/user")
+
 
 app.use(cors());
 app.use(express.json());
 app.use("/user", userRoute);
-app.use("/post", userRoute);
 
-mongoose
-  .connect(
-    "mongodb+srv://zhou:cc12112@cluster0.ebnlf4j.mongodb.net/MiniTwitter?retryWrites=true&w=majority"
-  )
-  .then(() => {
-    app.listen(3000, () => {
-      console.log("Mini Twitter is running at port 3000");
-    });
-    console.log("connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+
+
+mongoose.connect('mongodb+srv://zhou:cc12112@cluster0.ebnlf4j.mongodb.net/MiniTwitter?retryWrites=true&w=majority')
+.then(()=>{
+    app.listen(3000, ()=>{
+        console.log("Mini Twitter is running at port 3000")
+    })
+    console.log('connected to MongoDB')
+}).catch((error) => {
+    console.log(error)
+})
