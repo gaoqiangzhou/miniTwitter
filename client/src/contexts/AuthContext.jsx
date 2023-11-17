d2589608e134722d307355927a38e8f5cb087b57
gaoqiangzhou <gaoqiangzhouer@gmail.com>
2023-11-16 23:03:53 -0500

    added subscribe/unsubscribe functionality

--- client/src/contexts/AuthContext.jsx
+++ client/src/contexts/AuthContext.jsx
@@ -66,27 +66,14 @@ export function AuthContextProvider({children}){
         {
             console.log(err)
         }
-        // .then(res => {
-        //     if(res.data.status == "FAILED") throw new Error(res.data.message);
-        //     //save the user into local storage
-        //     const data = res.data;
-        //     const userInfo = {
-        //         name: data.data.name,
-        //         type: data.data.type,
-        //         _id: data.data._id,
-        //         token: data.token
-        //     }
-        //     localStorage.setItem('user', JSON.stringify(userInfo));
-            
-        //     //update user
-        //     setUser(userInfo);
-        //     setIsloading(false);
-        // })
-        // .catch(err => console.log(err))
+    }
+    const updateUser = (newUser) => {
+        setUser(newUser);
     }
     
     const values = {
         user: user,
+        updateUser: updateUser,
         signup: signup,
         logout: logout,
         login: login,
