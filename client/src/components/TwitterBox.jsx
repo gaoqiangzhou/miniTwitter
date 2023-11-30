5c77ede5d14df4f9ad38b17d964a280cdc48342c
Chr1sZzz <zhongzhuowen514@gmail.com>
2023-11-29 21:24:05 -0500

    still need to fix

--- client/src/components/TwitterBox.jsx
+++ client/src/components/TwitterBox.jsx
@@ -1,14 +1,11 @@
-import React, { useState , useEffect} from "react";
-import {
-  FaCommentAlt,
-  FaMoneyBillWaveAlt,
-  
-} from "react-icons/fa";
-import {FcLike, FcDislike} from "react-icons/fc"
+import React, { useState, useEffect } from "react";
+import { FaCommentAlt, FaMoneyBillWaveAlt } from "react-icons/fa";
+import { FcLike } from "react-icons/fc";
 import { useAuth } from "../contexts/AuthContext";
 import { SlUserFollow, SlUserFollowing } from "react-icons/sl";
 import axios from "axios";
 import { FcLikePlaceholder } from "react-icons/fc";
+import { RiDislikeFill, RiDislikeLine } from "react-icons/ri";
 
 //if user id == postuser id -> nothing
 const TwitterBox = (props) => {
@@ -19,14 +16,19 @@ const TwitterBox = (props) => {
   const DISLIKAPI = `http://localhost:3000/post/${props.postId}/dislike`;
   const DELETELIKAPI = `http://localhost:3000/post`;
 
-  
-
   const [likes, setLikes] = useState(props.initialLikes || []);
   const [dislikes, setDisLikes] = useState(props.initialDislikes || []);
-  const [isDisliked, setIsDisliked] = useState(props.initialDislikes.some((dislike) => dislike.user === userId));
-  const [isLiked, setIsLiked] = useState(likes.some((like) => like.user === userId));
-
-  
+  const [isDisliked, setIsDisliked] = useState(
+    props.initialDislikes.some((dislike) => dislike.user === userId)
+  );
+  const [isLiked, setIsLiked] = useState(
+    likes.some((like) => like.user === userId)
+  );
+  useEffect(() => {
+    // 在每次 likes 更新后，获取最新的 likes.length
+    const likesCount = likes.length;
+    console.log("Updated Likes Count:", likesCount);
+  }, [likes]);
 
   const subscribe = () => {
     axios
@@ -73,7 +75,15 @@ const TwitterBox = (props) => {
       .post(LIKAPI, { user: userId })
       .then((res) => {
         console.log(res);
-        setLikes([...likes, userId]);
+        const newLike = res.data.data;
+
+        // Check if the user has already liked the post
+        const userAlreadyLiked = likes.some((like) => like.user === userId);
+
+        // Update the state based on the actual data returned from the server
+        if (!userAlreadyLiked) {
+          setLikes((prevLikes) => [...prevLikes, newLike]);
+        }
       })
       .catch((err) => console.log(err));
   };
@@ -83,11 +93,21 @@ const TwitterBox = (props) => {
       .post(DISLIKAPI, { user: userId })
       .then((res) => {
         console.log(res);
-        setDisLikes([...dislikes, userId]);
-        setIsDisliked(true);
+        const newDislike = res.data.data;
+
+        // Check if the user has already disliked the post
+        const userAlreadyDisliked = dislikes.some(
+          (dislike) => dislike.user === userId
+        );
+
+        // Update the state based on the actual data returned from the server
+        if (!userAlreadyDisliked) {
+          setDisLikes((prevDislikes) => [...prevDislikes, newDislike]);
+        }
       })
       .catch((err) => console.log(err));
   };
+
   const handleUnlike = () => {
     axios
       .delete(`${DELETELIKAPI}/${props.postId}/like/${userId}`)
@@ -95,10 +115,12 @@ const TwitterBox = (props) => {
         console.log(res);
         // Assuming the response contains data about the deleted like
         const deletedLike = res.data.data;
-  
-        // Update the state to remove the unliked user from the likes array
-        const updatedLikes = likes.filter((like) => like._id !== deletedLike._id);
-        setLikes(updatedLikes);
+        console.log("New Likes:", likes);
+
+        // Update the state based on the actual data returned from the server
+        setLikes((prevLikes) =>
+          prevLikes.filter((like) => like._id !== deletedLike._id)
+        );
       })
       .catch((err) => {
         console.log(err);
@@ -112,11 +134,13 @@ const TwitterBox = (props) => {
       .then((res) => {
         console.log(res);
         // Assuming the response contains data about the deleted like
-        const deletedDisLike = res.data.data;
-  
-        // Update the state to remove the unliked user from the likes array
-        const updatedDisLikes = dislikes.filter((dislike) => dislike._id !== deletedDisLike._id);
-        setLikes(updatedDisLikes);
+        const removedDislike = res.data.data;
+        console.log("Removed Dislike:", removedDislike);
+
+        // Update the state based on the actual data returned from the server
+        setDisLikes((prevDislikes) =>
+          prevDislikes.filter((dislike) => dislike._id !== removedDislike._id)
+        );
       })
       .catch((err) => {
         console.log(err);
@@ -125,22 +149,21 @@ const TwitterBox = (props) => {
   };
   const handleLikeToggle = () => {
     if (isLiked) {
-      handleUnlike(); // 执行取消点赞的逻辑
+      handleUnlike();
     } else {
-      handlelike(); // 执行点赞的逻辑
+      handlelike();
     }
-    setIsLiked(!isLiked); // 切换 isLiked 的值
+    setIsLiked(!isLiked);
   };
+
   const handleDislikeToggle = () => {
     if (isDisliked) {
-      handleUndislike(); // 执行取消踩的逻辑
+      handleUndislike();
     } else {
-      handledislike(); // 执行踩的逻辑
+      handledislike();
     }
     setIsDisliked(!isDisliked);
   };
-
-  
 
   return (
     <div
@@ -178,29 +201,40 @@ const TwitterBox = (props) => {
       </div>
       <div className="mt-2">{props.content}</div>
       <div className="mt-4 flex">
-        <button
-          onClick={handleLikeToggle}
-          className={"mr-2 text-blue-500 hover:text-blue-700"}
-        >
-          {isLiked ? (
-            <>
-              <FcDislike /> Unlike{" "}
-            </>
-          ) : (
-            <>
-              <FcLikePlaceholder /> Like{" "}
-            </>
-          )}
-          
-        </button>
-        <button
-          onClick={handleDislikeToggle}
-          className={"mr-2 text-blue-500 hover:text-blue-700"}
-        >
-          {isDisliked ? <FcLikePlaceholder /> : <FcDislike />}{" "}
-          {isDisliked ? "Dislike" : "Undislike"}{" "}
-          
-        </button>
+        {userId !== props.userId && (
+          <>
+            <button
+              onClick={handleLikeToggle}
+              className="mr-2 text-blue-500 hover:text-blue-700"
+            >
+              {isLiked ? (
+                <FcLike /> // Unlike icon when the post is liked
+              ) : (
+                <FcLikePlaceholder /> // Like icon when the post is not liked
+              )}
+              {isLiked ? " Like" : " Unlike"}
+            </button>
+            <span className="badge">{likes.length}</span>
+          </>
+        )}
+
+        {userId !== props.userId && (
+          <>
+            <button
+              onClick={handleDislikeToggle}
+              className="mr-2 text-red-500 hover:text-red-700"
+            >
+              {isDisliked ? (
+                <RiDislikeFill /> // Remove dislike icon when the post is disliked
+              ) : (
+                <RiDislikeLine /> // Add dislike icon when the post is not disliked
+              )}
+              {isDisliked ? " Dislike" : " Undislike"}
+            </button>
+            <span className="badge">{dislikes.length}</span>
+          </>
+        )}
+
         <button className="mr-2 text-blue-500 hover:text-blue-700">
           <FaCommentAlt /> comment
         </button>
