7164bfab93ef8be8435c9c23e9de30c2f5f9cbb1
gaoqiangzhou <gaoqiangzhouer@gmail.com>
2023-12-06 11:06:32 -0500

    dispute post hook added

--- server/Models/disputeMessage.js
+++ server/Models/disputeMessage.js
@@ -15,8 +15,34 @@ const disputeMessageSchema = new mongoose.Schema({
         required: true,
     },
     approved:{
-        type: Boolean,
-        default: null,
+        type: String,
+        default: "Unprocessed"
+    }
+})
+disputeMessageSchema.post("findOneAndUpdate", async (doc, next) => {
+    try{
+        if(doc.approved === "Yes")
+        {
+            //get complaint type
+            let data = await doc   
+            .model("Complaint")
+            .findOne({_id: doc.complain})
+            console.log(data)
+            if(data.complaintType === "post")
+            {
+                //remove both complaint from post and user
+                await doc   
+                .model("Complaint")
+                .findOneAndRemove({_id: doc.complain})
+                await doc
+                .model("User")
+                .findOneAndUpdate({_id: doc.from}, {$pull: {warns: doc.complain}})
+            }
+        }
+    }catch (error)
+    {
+        console.log("get error: ", error);
+        next(error)
     }
 })
 const DisputeMessage = new mongoose.model("DisputeMessage", disputeMessageSchema);
